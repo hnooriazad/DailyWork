b8ad10e043e647736ee4f9cf1b018d8012c3dea1
HosseiN NooriAzad <h.nooriazad93@gmail.com>
2017-04-08 15:53:21 +0430

    add table  in start
    اضافه کردن جدول برای شروع

--- src/hna/pro/dailywork/MainActivity.java
+++ src/hna/pro/dailywork/MainActivity.java
@@ -2,15 +2,38 @@ package hna.pro.dailywork;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	 private TableLayout tableLayout;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		  tableLayout=(TableLayout)findViewById(R.id.tableLayout);
+
+	        for (int i=0;i<5;i++){
+	            View tableRow = LayoutInflater.from(this).inflate(R.layout.table_item,null,false);
+	            TextView history_display_no  = (TextView) tableRow.findViewById(R.id.history_display_no);
+	            TextView history_display_date  = (TextView) tableRow.findViewById(R.id.history_display_date);
+	            TextView history_display_orderid  = (TextView) tableRow.findViewById(R.id.history_display_orderid);
+	            TextView history_display_quantity  = (TextView) tableRow.findViewById(R.id.history_display_quantity);
+
+	            history_display_no.setText(""+(i+1));
+	            history_display_date.setText("2014-02-05");
+	            history_display_orderid.setText("S0"+(i+1));
+	            history_display_quantity.setText(""+(20+(i+1)));
+	            tableLayout.addView(tableRow);
+	        }
+
 	}
 
 	@Override
